1bfc15a7b3100235e82bd42775f5dc63cfb60abb
Ezgi Nur UCAY <ezginrucay@gmail.com>
2019-08-01 15:24:45 +0300

    Update LoginOperations.java

--- com/LinkedInAPI/controller/LoginOperations.java
+++ com/LinkedInAPI/controller/LoginOperations.java
@@ -13,6 +13,7 @@ import com.linkedinAPI.model.LinkedinConnectionStrings;
 public class LoginOperations extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	// To good development you should define a config file to stora Connection String.
 	public static String URL = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id="
 			+ LinkedinConnectionStrings.getClient_id() + "&redirect_uri=" + LinkedinConnectionStrings.getRedirect_url()
 			+ "&state=fdfdfdfd&scope=r_liteprofile%20r_emailaddress";
